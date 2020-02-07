a41a138b80afb13ad84bd9828c66d96b24b3d81e
mirel <mirel.lazar@gmail.com>
2020-02-07 18:19:21 +0100

    add dir listing and file content test

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # fatfs_psx
 embedded fatfs/exfat implementation for psx
+
+original implementation comes from http://elm-chan.org/fsw/ff/00index_e.html
+check ffconf.h for enabled functionality and change if/as needed
+
+for PS3, use the sdk from https://github.com/Estwald/PSDK3v2 or any other compatible sdk and build & run the included example
+
+provided 'as is' with no guarantee or warranties. check original LICENSE.txt for details.
+
+cheers
--- ps3_example/source/main.c
+++ ps3_example/source/main.c
@@ -52,9 +52,36 @@ typedef struct {
 #include <sysutil/disc.h>
 
 #include <sysmodule/sysmodule.h>
+//app
+int app_init (int dt);      //1
+int app_input (int dt);     //2
+int app_update (int dt);    //3
+int app_render (int dt);    //4
+int app_cleanup (int dt);   //5
+//
+//pad buttons mask - for use in app
+#define PAD_R1_MASK         0x0001
+#define PAD_L1_MASK         0x0002
+#define PAD_R2_MASK         0x0004
+#define PAD_L2_MASK         0x0008
+#define PAD_CR_MASK         0x0010
+#define PAD_SQ_MASK         0x0020
+#define PAD_CI_MASK         0x0040
+#define PAD_TR_MASK         0x0080
+#define PAD_SE_MASK         0x0100
+#define PAD_ST_MASK         0x0200
+#define PAD_R3_MASK         0x0400
+#define PAD_L3_MASK         0x0800
+#define PAD_UP_MASK         0x1000
+#define PAD_DN_MASK         0x2000
+#define PAD_LT_MASK         0x4000
+#define PAD_RT_MASK         0x8000
+//
 
 int fddr[8] = {0};
 char fdld[8][256];
+char fn[256] = {0};
+char dn[256] = {0};
 static u64 ff_ps3id[8] = {
 	0x010300000000000AULL, 0x010300000000000BULL, 0x010300000000000CULL, 0x010300000000000DULL,
 	0x010300000000000EULL, 0x010300000000000FULL, 0x010300000000001FULL, 0x0103000000000020ULL 
@@ -74,6 +101,7 @@ int sdopen (int fd)
     return ret;
 }
 #else
+//tools
 FRESULT scan_files2 (
     char* path        /* Start node to be scanned (***also used as work area***) */
 )
@@ -120,39 +148,36 @@ FRESULT scan_files (
 {
     FRESULT res;
     FDIR dir;
-    UINT i = 0;
     static FILINFO fno;
 
-    DPrintf("Listing path contents for '%s'", path);
     res = f_opendir(&dir, path);                       /* Open the directory */
     if (res == FR_OK)
     {
-        for (i = 0; i < 8; i++)
+        DPrintf("Listing path contents for '%s'\n", path);
+        for (;;)
         {
             FRESULT res1 = f_readdir(&dir, &fno);                   /* Read a directory item */
             if (res1 != FR_OK || fno.fname[0] == 0) 
             {
-                //res1 = disk_status(dir.obj.fs->pdrv);
-                DPrintf("%x !f_readdir %s drive %d ssize %d\n", res1, path, dir.obj.fs->pdrv, 0/*dir.obj.fs->ssize*/);
-                //
                 f_closedir(&dir);
                 return res;  /* Break on error or end of dir */
             }
             if (fno.fattrib & AM_DIR) 
             {                    /* It is a directory */
                 DPrintf("/%s\n", fno.fname);
-                //sprintf(&path[i], "/%s", fno.fname);
-                //res = scan_files(path);                    /* Enter the directory */
-                //if (res != FR_OK) break;
-                //path[i] = 0;
+                snprintf (dn, 255, "%s%s", path, fno.fname);
             } 
             else 
             {                                       /* It is a file. */
                 DPrintf("%s\n", fno.fname);
-                //printf("%s/%s\n", path, fno.fname);
+                snprintf (fn, 255, "%s%s", path, fno.fname);
             }
         }
         f_closedir(&dir);
+    }
+    else
+    {
+        DPrintf("!unable to open path '%s' result %d\n", path, res);
     }
     return res;
 }
@@ -161,15 +186,13 @@ int sdopen (int i)
 {
     //FDIR fdir;
     char lbuf[10];
-    FATFS *fs;     /* Ponter to the filesystem object */
-    fs = malloc(sizeof (FATFS));           /* Get work area for the volume */
+    FATFS fs;     /* Ponter to the filesystem object */
     snprintf(lbuf, 10, "%d:/", i);
-    f_mount(fs, lbuf, 0);                    /* Mount the default drive */
-    int ret = scan_files(lbuf); //f_opendir (&fdir, lbuf);
-    //if (ret == FR_OK)
-    //    f_closedir (&fdir);
-    f_mount(0, lbuf, 0);                    /* Mount the default drive */
-    free(fs);
+    int ret = f_mount(&fs, lbuf, 0);                    /* Mount the default drive */
+    if (ret != FR_OK)
+        return ret;
+    ret = scan_files(lbuf); //f_opendir (&fdir, lbuf);
+    f_mount(NULL, lbuf, 0);                    /* UnMount the default drive */
     //
     return ret;
 }
@@ -208,12 +231,22 @@ int fatfs_init()
         //f_mount(fs, lbuf, 0);                    /* Mount the default drive */
         fdld[k][0] = '\0';
         fddr[k] = sdopen(k);
-        DPrintf("drive %d open result %d for 0x%llx\n", k, fddr[k], (long long unsigned int)ff_ps3id[k]);
+        DPrintf("open drive %d result %d for 0x%llx\n", k, fddr[k], (long long unsigned int)ff_ps3id[k]);
         //fddr[i] = f_opendir (&fdir, lbuf);
         //if (fddr[i] == FR_OK)
         //    f_closedir (&fdir);
         //f_mount(0, lbuf, 0);                    /* Mount the default drive */
     }
+    //
+    if (*fn)
+    {
+        DPrintf("\npress triangle to list contents of file '%s'\n", fn);
+    }
+    if (*dn)
+    {
+        DPrintf("\npress rectangle to list contents of dir '%s'\n", dn);
+    }
+    //
     return 0;
 }
 #endif
@@ -236,8 +269,143 @@ void LoadTexture()
 
     // here you can add more textures using 'texture_pointer'. It is returned aligned to 16 bytes
 }
+//dir contents
+int dir_read (char *dname)
+{
+    FRESULT res;
+    FDIR dir;
+    static FILINFO fno;
+    /* Register work area to the default drive */
+    FATFS fs;   /* Work area (filesystem object) for logical drive */
+    char drn[5];
+    snprintf(drn, 4, "%.3s", dname);
+    DPrintf("mounting drive '%s'\n", drn);
+    DPrintf("listing directory '%s':\n", dname);
+    f_mount(&fs, drn, 0);
+
+    res = f_opendir(&dir, dname);                       /* Open the directory */
+    if (res == FR_OK)
+    {
+        for (;;)
+        {
+            FRESULT res1 = f_readdir(&dir, &fno);                   /* Read a directory item */
+            if (res1 != FR_OK || fno.fname[0] == 0) 
+            {
+                f_closedir(&dir);
+                return res;  /* Break on error or end of dir */
+            }
+            if (fno.fattrib & AM_DIR) 
+            {                    /* It is a directory */
+                DPrintf("/%s\n", fno.fname);
+                snprintf (dn, 255, "%s%s/", dname, fno.fname);
+            } 
+            else 
+            {                                       /* It is a file. */
+                DPrintf("%s\n", fno.fname);
+                snprintf (fn, 255, "%s%s", dname, fno.fname);
+            }
+        }
+        f_closedir(&dir);
+    }
+    else
+    {
+        DPrintf("!unable to open path '%s' result %d\n", dname, res);
+    }
+    //
+    f_mount(0, drn, 0);
+    //
+    return res;
+}
+
+int dir_run(char *dname)
+{
+    if(!dname || !*dname)
+        return 0;
+    //debug console
+    initConsole ();
+    DbgHeader("DIR contents");
+    DbgMess("Press o/circle to return");
+    //
+    dir_read (dname);
+    //
+    int btn = 0;
+    //
+    while(1)
+    {
+        //2 input
+        btn = app_input(0);
+        if (btn & PAD_CI_MASK)
+            break;
+        //3
+		//4
+        DbgDraw();
+        tiny3d_Flip();
+    }
+    return 0;
+}
+
+//file contents
+int file_read(char *fname)
+{
+    FIL fil;        /* File object */
+    char line[100]; /* Line buffer */
+    FRESULT fr;     /* FatFs return code */
+    FATFS fs;   /* Work area (filesystem object) for logical drive */
+
+    /* Register work area to the default drive */
+    char dn[5];
+    snprintf(dn, 4, "%.3s", fname);
+    DPrintf("mounting drive '%s'\n", dn);
+    DPrintf("reading file '%s':\n", fname);
+    f_mount(&fs, dn, 0);
+
+    /* Open a text file */
+    fr = f_open(&fil, fname, FA_READ);
+    if (fr) return (int)fr;
+
+    /* Read every line and display it */
+    while (f_gets(line, sizeof (line), &fil)) 
+    {
+        DPrintf(line);
+    }
+
+    /* Close the file */
+    f_close(&fil);
+    //
+    f_mount(0, dn, 0);
+    //
+    return 0;
+}
+
+int file_run(char *fname)
+{
+    if(!fname || !*fname)
+        return 0;
+    //debug console
+    initConsole ();
+    DbgHeader("File contents");
+    DbgMess("Press o/circle to return");
+    //
+    file_read (fname);
+    //
+    int btn = 0;
+    //
+    while(1)
+    {
+        //2 input
+        btn = app_input(0);
+        if (btn & PAD_CI_MASK)
+            break;
+        //3
+		//4
+        DbgDraw();
+        tiny3d_Flip();
+    }
+    return 0;
+}
+//app
 //1st
-int app_init (int dat)
+int app_init (int dt)
 {
     //debug console
     initConsole ();
@@ -257,26 +425,80 @@ int app_init (int dat)
     return 1;
 }
 //2nd
+#if 0
+from
+unsigned int BTN_LEFT : 1;      /*!< \brief left button */
+unsigned int BTN_DOWN : 1;      /*!< \brief down button */
+unsigned int BTN_RIGHT : 1;     /*!< \brief right button */
+unsigned int BTN_UP : 1;        /*!< \brief up button */
+unsigned int BTN_START : 1;     /*!< \brief start button */
+unsigned int BTN_R3 : 1;        /*!< \brief R3 button */
+unsigned int BTN_L3 : 1;        /*!< \brief L3 button */
+unsigned int BTN_SELECT : 1;    /*!< \brief select button */
+unsigned int BTN_SQUARE : 1;    /*!< \brief square button */
+unsigned int BTN_CROSS : 1;     /*!< \brief cross button */
+unsigned int BTN_CIRCLE : 1;    /*!< \brief circle button */
+unsigned int BTN_TRIANGLE : 1;  /*!< \brief triangle button */
+unsigned int BTN_R1 : 1;        /*!< \brief R1 button */
+unsigned int BTN_L1 : 1;        /*!< \brief L1 button */
+unsigned int BTN_R2 : 1;        /*!< \brief R2 button */
+unsigned int BTN_L2 : 1;        /*!< \brief L2 button */ 
+to 
+#define PAD_R1_MASK         0x0001
+#define PAD_L1_MASK         0x0002
+#define PAD_R2_MASK         0x0004
+#define PAD_L2_MASK         0x0008
+#define PAD_CR_MASK         0x0010
+#define PAD_SQ_MASK         0x0020
+#define PAD_CI_MASK         0x0040
+#define PAD_TR_MASK         0x0080
+#define PAD_SE_MASK         0x0100
+#define PAD_ST_MASK         0x0200
+#define PAD_R3_MASK         0x0400
+#define PAD_L3_MASK         0x0800
+#define PAD_UP_MASK         0x1000
+#define PAD_DN_MASK         0x2000
+#define PAD_LT_MASK         0x4000
+#define PAD_RT_MASK         0x8000
+#endif
 int app_input(int dat)
 {
 	padInfo padinfo;
 	padData paddata;
     int i;
+    int ret = 0;
     // Check the pads.
     ioPadGetInfo(&padinfo);
 
-    for(i = 0; i < MAX_PADS; i++){
-
-        if(padinfo.status[i]){
+    for(i = 0; i < MAX_PADS; i++)
+    {
+        if(padinfo.status[i])
+        {
             ioPadGetData(i, &paddata);
-            
-            if(paddata.BTN_CROSS){
-                return 0;
-            }
+            //map buttons
+            if(paddata.BTN_UP)
+                ret |= PAD_UP_MASK;
+            if(paddata.BTN_DOWN)
+                ret |= PAD_DN_MASK;
+            if(paddata.BTN_LEFT)
+                ret |= PAD_LT_MASK;
+            if(paddata.BTN_RIGHT)
+                ret |= PAD_RT_MASK;
+            if(paddata.BTN_CROSS)
+                ret |= PAD_CR_MASK;
+            if(paddata.BTN_CIRCLE)
+                ret |= PAD_CI_MASK;
+            if(paddata.BTN_TRIANGLE)
+                ret |= PAD_TR_MASK;
+            if(paddata.BTN_SQUARE)
+                ret |= PAD_SQ_MASK;
+            if(paddata.BTN_START)
+                ret |= PAD_ST_MASK;
+            if(paddata.BTN_SELECT)
+                ret |= PAD_SE_MASK;
         }
-        
     }
-    return 1;
+    return ret;
 }
 //3rd
 int app_update(int dat)
@@ -296,19 +518,53 @@ int app_render(int dat)
 //5th
 int app_cleanup(int dat)
 {
+    ioPadEnd();
+
     return 1;
 }
-
+//restore app state after other module was executed
+int _app_restore ()
+{
+    initConsole ();
+    DbgHeader("FATFS EXFAT Example");
+    DbgMess("Press o/circle to exit");
+    if (*fn)
+    {
+        DPrintf("\npress triangle to list contents of file '%s'\n", fn);
+    }
+    if (*dn)
+    {
+        DPrintf("\npress rectangle to list contents of dir '%s'\n", dn);
+    }
+    return 0;
+}
+//
 s32 main(s32 argc, const char* argv[])
 {
     //1 init
 	app_init(0);
+    int btn = 0;
 	// Ok, everything is setup. Now for the main loop.
 	while(1) 
     {
         //2 input
-        if (!app_input(0))
-            return 0;
+        btn = app_input(0);
+        if (btn & PAD_CI_MASK)
+            break;
+        //file contents
+        else if(btn & PAD_TR_MASK)
+        {
+            file_run(fn);
+            //
+            _app_restore();
+        }
+        //dir listing
+        else if(btn & PAD_SQ_MASK)
+        {
+            dir_run(dn);
+            //
+            _app_restore();
+        }
         //3
         app_update(0);
 		//4
